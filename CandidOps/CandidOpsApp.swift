30ca38464ac325008dde85a2c5d2c002ced6ee0c
Edward Han <edwrdhn@gmail.com>
2024-07-20 16:07:13 -0400

    Initial Commit

---
+++ CandidOps/CandidOpsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CandidOpsApp.swift
+//  CandidOps
+//
+//  Created by Edward Han on 7/20/24.
+//
+
+import SwiftUI
+
+@main
+struct CandidOpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
